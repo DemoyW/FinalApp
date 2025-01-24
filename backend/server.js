216782582de16c6530a41ba29f94b1fbcee1e0a2
DemoyW <d.witter@se22.qmul.ac.uk>
2025-01-24 21:58:59 +0000

    posting to user table

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import User from "./models/user.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
+app.post("/users",  async (req, res) => {
+    const user = req.body; // req.body is the data that is sent with the POST request
+
+    if(!user.usernaem || !user.password) {
+        return res.status(400).json({ success:false, message: "Please provide a username and password" });
+    }
+
+    const newUser = new User(user)
+
+    try {
+        await newUser.save();
+        res.status(201).json({ success: true, data: newUser });
+    } catch (error) {
+        console.error("Error in create user", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+
 });
 
 
