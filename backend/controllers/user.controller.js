b3c9282e3eee37e5138f8650e478507edd7e9404
DemoyW <d.witter@se22.qmul.ac.uk>
2025-01-26 02:08:47 +0000

    fixing controllers

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,63 @@
+import express from "express";
+import User from "../models/user.model.js";
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        console.error("Error in fetching users", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    } 
+};
+
+export const createUser = async (req, res) => {
+    const user = req.body; // req.body is the data that is sent with the POST request
+
+    if(!user.username || !user.password) {
+        return res.status(400).json({ success:false, message: "Please provide a username and password" });
+    }
+
+    const newUser = new User(user)
+
+    try {
+        await newUser.save();
+        res.status(201).json({ success: true, data: newUser });
+    } catch (error) {
+        console.error("Error in create user", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+};    
+
+export const updateUser =  async (req, res) => { 
+        const { id } = req.params;
+        const user = req.body;
+    
+        if(!mongoose.Types.ObjectId.isValid(id)) { // Check if the id is a valid ObjectId
+            return res.status(404).json({ success: false, message: "Invalid user id" });
+        }
+    
+        try {
+            const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
+            res.status(200).json({ success: true, data: updatedUser });
+        } catch (error) {
+            console.error("Error in update user", error.message);
+            res.status(500).json({ success: false, message: "Server Error" });
+        }
+    };
+
+export const deleteUser =  async (req, res) => {
+const { id } = req.params;
+    
+    try {
+        const deletedUser = await User.findByIdAndDelete(id);
+
+
+        res.status(200).json({ success: true, message: "User deleted" });
+    } catch (error) {
+        console.error("Error in delete user", error.message);
+        res.status(404).json({ success: false, message: "User not found" });
+    }
+};
+        
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,69 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
-import User from "../models/user.model.js";
+import { createUser, deleteUser, getUsers, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const users = await User.find({});
+router.get("/", getUsers);
 
-        res.status(200).json({ success: true, data: users });
-    } catch (error) {
-        console.error("Error in fetching users", error.message);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-} );
+router.post("/",  createUser);
 
-router.post("/",  async (req, res) => {
-    const user = req.body; // req.body is the data that is sent with the POST request
+router.put("/:id", updateUser);
 
-    if(!user.username || !user.password) {
-        return res.status(400).json({ success:false, message: "Please provide a username and password" });
-    }
-
-    const newUser = new User(user)
-
-    try {
-        await newUser.save();
-        res.status(201).json({ success: true, data: newUser });
-    } catch (error) {
-        console.error("Error in create user", error.message);
-        res.status(500).json({ success: false, message: "Server error" });
-    }
-
-});
-
-router.put("/:id", async (req, res) => { 
-    const { id } = req.params;
-    const user = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) { // Check if the id is a valid ObjectId
-        return res.status(404).json({ success: false, message: "Invalid user id" });
-    }
-
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
-        res.status(200).json({ success: true, data: updatedUser });
-    } catch (error) {
-        console.error("Error in update user", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
-
-router.delete("/:id", async (req, res) => {
-const { id } = req.params;
-    
-    try {
-        const deletedUser = await User.findByIdAndDelete(id);
-
-
-        res.status(200).json({ success: true, message: "User deleted" });
-    } catch (error) {
-        console.error("Error in delete user", error.message);
-        res.status(404).json({ success: false, message: "User not found" });
-    }
-});
+router.delete("/:id",  deleteUser);
 
 
 export default router;
