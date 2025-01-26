a19114690e7b58e26fdaaaf5e69b04f203a6bc4f
DemoyW <d.witter@se22.qmul.ac.uk>
2025-01-26 22:41:41 +0000

    adding global storage

---
+++ frontend/AmpliFitness/store/user.js
@@ -0,0 +1,21 @@
+import {create} from zustand;
+
+export const useUserStore = create((set) => ({
+    users: [],
+    setUsers: (users) => set({ users }),
+    createUser: async (newUser) => {
+        if(!newUser.username || !newUser.password) {
+            return { success: false, message: "Please provide a username and password" };
+        }
+        const res = await fetch("http://localhost:8000/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser),
+        })
+        const data = await res.json();
+        set((state) => ({ users: [...state.users, data.data] }));
+    },
+}));
+
