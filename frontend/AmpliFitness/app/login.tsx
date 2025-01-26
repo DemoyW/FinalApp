d533dd10d3e28f1f81ee5a5b41a1f1c77079e510
DemoyW <d.witter@se22.qmul.ac.uk>
2025-01-26 21:10:31 +0000

    adding state

--- frontend/AmpliFitness/app/login.tsx
+++ frontend/AmpliFitness/app/login.tsx
@@ -1,20 +1,46 @@
 import {Text, View, Button, StyleSheet, TextInput} from "react-native";
 import {Link} from "expo-router";
 
+import { useState } from "react";
+
 export default function LoginScreen() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+ 
+
+  const handleLogin = () => {
+    console.log("Logging in with", user.email, user.password);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to AmpliFitness</Text>
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput style={styles.input} placeholder="Password" />
-      <Button title="Log In" onPress={() => console.log("Log In")} />
+      <TextInput 
+        style={styles.input} 
+        placeholder="Email" 
+        placeholderTextColor={"grey"}
+        value={user.email} 
+        onChangeText={(email) => setUser({ ...user, email })}
+      />
+      <TextInput
+        style={styles.input} 
+        placeholder="Password"
+        placeholderTextColor={"grey"}
+        value={user.password}
+        onChangeText={(password) => setUser({ ...user, password })}
+        secureTextEntry={true} 
+      />
+      <Button title="Log In" onPress={handleLogin} />
+     
       <Button title="Sign Up" onPress={() => console.log("Sign Up")} />
 
       <Link href="/(tabs)/home" style={styles.button}>
        Home
         </Link>
     </View>
-  );
+  );  
 } 
 
 const styles = StyleSheet.create({
@@ -22,6 +48,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "lightblue",
   },
   text: {
     fontSize: 20,
@@ -33,6 +60,7 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    color: "black",
   }, 
   button: {
     fontSize: 20,
