3405a64c245d5cc3cc4b96ec8c66974825598bc1
DemoyW <d.witter@se22.qmul.ac.uk>
2025-02-06 00:25:54 +0000

    Adding the workout and exercise models

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const exerciseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+});
