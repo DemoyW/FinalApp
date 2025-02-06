1762eca2ba2980bcc2d35d91422fcce1cf17bda3
DemoyW <d.witter@se22.qmul.ac.uk>
2025-02-06 13:21:07 +0000

    Adding the journal screen

---
+++ frontend/AmpliFitness/app/(tabs)/journal.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function JournalScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Journal</Text>
+            <Text>Keep track of progress pictures</Text>
+        </View>
+    );
+    }
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightblue',
+    },
+});
+    
