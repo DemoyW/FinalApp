a000cd6ef12149e2b38907b52fc4686154a78190
DemoyW <d.witter@se22.qmul.ac.uk>
2025-02-25 10:16:05 +0000

    displaying trainers(users)

--- frontend/AmpliFitness/app/(tabs)/trainers.tsx
+++ frontend/AmpliFitness/app/(tabs)/trainers.tsx
@@ -1,14 +1,50 @@
 import {Text, View, StyleSheet} from 'react-native';
+import { useEffect, useState } from 'react';
+import { useUserStore } from '@/store/user';
+import { FlatList, GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+
+interface Item {
+    _id: string;
+    username: string;
+}
 
 export default function TrainersScreen() {
+    const [trainers, setTrainers] = useState<Item[]>([]);
+    const { getAllUsers } = useUserStore();
+
+    const fetchUsers = async () => {
+        try {
+            const allUsers = await getAllUsers();
+            console.log("allUsers", allUsers);
+            setTrainers(allUsers.message.data);
+            console.log("Trainers", trainers);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        fetchUsers();
+    }
+    , []);
+
     return (
+    <GestureHandlerRootView>
         <View style={styles.container}>
-        <Text style={styles.text} >Trainers</Text>
-        <Text style={styles.text}>Find a trainer to help you reach your fitness goals</Text>
-        <Text style={styles.text}>Trainer 1</Text>
-        <Text style={styles.text}>Trainer 2</Text>
-        <Text style={styles.text}>Trainer 3</Text>
+            <Text style={styles.text} >Trainers</Text>
+            <Text style={styles.text}>Find a trainer to help you reach your fitness goals</Text>
+            <FlatList
+                data={trainers}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => <View>
+                    <Text style={styles.text}>{item.username}</Text>
+                </View>}
+            />
         </View>
+    </GestureHandlerRootView>
+
+        
     );
     }
 
